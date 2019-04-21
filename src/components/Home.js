c760eefedbf4158bd61f6c8c3fd908ded061de85
Joe LoPresti <jlopresti87@gmail.com>
2019-04-21 09:56:51 -0400

    Update homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return ( 
-        <h1>Hello World</h1>
+        <>
+        <h1>Welcome to manbearcave.com</h1>
+        <img
+          src="https://njkx.imgeng.in/cr_950,450,0,400/https://images.pexels.com/photos/749317/pexels-photo-749317.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" 
+          alt ="woods"
+          />
+          <br />
+        <Link to='/shop'>
+        <button>Store</button>
+        </Link>
+        </>
      );
 }
  
