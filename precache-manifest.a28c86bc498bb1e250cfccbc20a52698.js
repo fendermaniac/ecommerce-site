58b66926bb977a9fc52a7326e8dc2836a7155c71
Joe LoPresti <jlopresti87@gmail.com>
2019-04-23 21:01:19 -0400

    Updates

---
+++ precache-manifest.a28c86bc498bb1e250cfccbc20a52698.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0a847691386eb891549a3801b547c79",
+    "url": "/ecommerce-site/static/media/bear.a0a84769.svg"
+  },
+  {
+    "revision": "a89f23fa4bf9d6e0fcba",
+    "url": "/ecommerce-site/static/js/runtime~main.ec3a0902.js"
+  },
+  {
+    "revision": "a1aa9a77f3fcbe44725d",
+    "url": "/ecommerce-site/static/js/main.72f9d0c2.chunk.js"
+  },
+  {
+    "revision": "96254ab59c3724352c14",
+    "url": "/ecommerce-site/static/js/2.7a713d31.chunk.js"
+  },
+  {
+    "revision": "a1aa9a77f3fcbe44725d",
+    "url": "/ecommerce-site/static/css/main.ec7ca3e4.chunk.css"
+  },
+  {
+    "revision": "02624badb0afc0d02879e66e2d4332e2",
+    "url": "/ecommerce-site/index.html"
+  }
+];
